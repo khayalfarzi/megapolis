83a3678e951c73e9d5d409d31941313e7da1f60d
khayalfarzi <khayal.farziyev@pashabank.az>
2022-09-11 23:04:48 +0400

    refactored repository

---
+++ src/main/java/com/company/citylist/repository/CityRepository.java
@@ -0,0 +1,13 @@
+package com.company.citylist.repository;
+
+import com.company.citylist.domain.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+
+    Optional<City> findByName(String name);
+}
